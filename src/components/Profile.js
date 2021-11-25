6a240e47da851ea19e25566f5542948fc0e1195d
manasasakamuri <sakamurimanasa7176@gmail.com>
2021-11-25 16:44:47 +0530

    initial commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 function Profile() {
-    return (
-        <div>
-            <h1> Profile Page </h1>
-            <p> Name: </p>
-            <p> Age: </p>
-            <p> Email: </p>
-        </div>
-    )
+  const user = useSelector((state) => state.user.value);
+  const themeColor = useSelector((state) => state.theme.value);
+
+  return (
+    <div style={{ color: themeColor }}>
+      <h1> Profile Page</h1>
+      <p> Name: {user.name} </p>
+      <p> Age: {user.age}</p>
+      <p> Email: {user.email}</p>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
